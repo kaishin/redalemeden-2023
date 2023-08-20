701905c6cf3a22bd4a3126c0fbafe71da250c514
Reda Lemeden <git@redalemeden.com>
2023-08-20 18:36:04 +0200

    Implement main layout

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+const maxSideBarWidth = '20rem';
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			gridTemplateColumns: {
+				'main': `${maxSideBarWidth} 1fr`,
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography')
+	],
+}
