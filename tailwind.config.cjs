a1a4b49895ffb48f2c9d3566413890f54e90fb3e
Reda Lemeden <git@redalemeden.com>
2023-10-05 16:57:28 +0200

    Update packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,15 @@
 const maxSideBarWidth = '20rem';
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			gridTemplateColumns: {
-				'main': `${maxSideBarWidth} 1fr`,
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'main': `${maxSideBarWidth} 1fr`,
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
